d69ea47e5a605f3fa64d29cd30959f0b8f2024dc
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-11-18 09:36:14 +0100

    Rename activity

---
+++ app/src/main/java/ch/heigvd/labo3/fragments/Notes.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.labo3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class Notes : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var l = 2
+    }
+}
