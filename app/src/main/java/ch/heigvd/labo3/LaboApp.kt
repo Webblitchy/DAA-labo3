21beaaa0bdab9c2a70d9661a5829c00a72f6d279
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-11-28 15:15:53 +0100

    Add LaboApp

---
+++ app/src/main/java/ch/heigvd/labo3/LaboApp.kt
@@ -0,0 +1,16 @@
+package ch.heigvd.labo3
+
+import android.app.Application
+import ch.heigvd.labo3.database.LaboDatabase
+import ch.heigvd.labo3.database.Repository
+
+// Singleton which stocks the reference to the database
+class LaboApp : Application() {
+    //private val applicationScope = CoroutineScope(SupervisorJob())
+
+    val repository by lazy {
+        val database = LaboDatabase.getDatabase(this)
+        Repository(database.noteDAO()/*, applicationScope*/)
+    }
+
+}
